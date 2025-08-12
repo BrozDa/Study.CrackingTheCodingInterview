2ea5e94f4acd3ef1003f55cc74506ed693d41f7c
Brozda <daniel.brozik@gmail.com>
2025-08-12 18:16:06 +0200

    feat: Ch1 Q3 done and tested

---
+++ src/Study.CrackingTheCodingInterview/Ch1_ArraysAndStrings/Q3_Urlify.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.CrackingTheCodingInterview.Ch1_ArraysAndStrings
+{
+    //replace all spaces with "%20"
+    //Constraints: string has sufficient space to hold additional characters
+    //Bonus - no additional DS
+    // Example: input (char arr) "Mr_John_Smith____", truelen = 13 (without the end buffer)
+    public static class Q3_Urlify
+    {
+        public static string UrlifyOpt1(char[] input, int trueLen)
+        {
+            int endIndex = input.Length - 1;
+            int charIndex = trueLen-1;
+
+            while(charIndex >= 0)
+            {
+                if (input[charIndex] == ' ')
+                {
+                    input[endIndex--] = '0';
+                    input[endIndex--] = '2';
+                    input[endIndex--] = '%';
+                }
+                else
+                {
+                    input[endIndex--] = input[charIndex];
+                }
+                charIndex--;
+            }
+
+            return new string(input);
+            
+
+        }
+
+    }
+}
---
+++ tests/Study.CrackingTheCodingInterview.UnitTests/Ch1_ArraysAndStrings/Q3_UrlifyTests.cs
@@ -0,0 +1,44 @@
+﻿using Study.CrackingTheCodingInterview.Ch1_ArraysAndStrings;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Study.CrackingTheCodingInterview.UnitTests.Ch1_ArraysAndStrings
+{
+    public class Q3_UrlifyTests
+    {
+        private readonly char[] request1 = "Hi   ".ToCharArray();
+        private readonly int trueLen1 = 3;
+        private readonly string result1 = "Hi%20";
+
+        private readonly char[] request2 = "Mr John Smith    ".ToCharArray();
+        private readonly int trueLen2 = 13;
+        private readonly string result2 = "Mr%20John%20Smith";
+
+        [Fact]
+        public void UrlifyOpt1_Request1_ReturnsResult1()
+        {
+            //arrange
+            //----
+
+            //act
+            var result = Q3_Urlify.UrlifyOpt1(request1,trueLen1);
+            //assert
+            Assert.Equal(result1, result);
+        }
+
+        [Fact]
+        public void UrlifyOpt1_Request2_ReturnsResult2()
+        {
+            //arrange
+            //----
+
+            //act
+            var result = Q3_Urlify.UrlifyOpt1(request2, trueLen2);
+            //assert
+            Assert.Equal(result2, result);
+        }
+
+
+    }
+}
