40b9b60266ca89b662699a98f602a38c31849432
Brozda <daniel.brozik@gmail.com>
2025-08-12 17:26:54 +0200

    feat: Ch1 Q2 done and tested

---
+++ src/Study.CrackingTheCodingInterview/Ch1_ArraysAndStrings/Q2_CheckPermutation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.CrackingTheCodingInterview.Ch1_ArraysAndStrings
+{
+    //given two strings check if they are permutation one of another test, tset - permutations
+    //constaints - only lower characters
+    public static class Q2_CheckPermutation
+    {
+        public static bool IsPermutationOpt1(string first, string second)
+        {
+            //Efficient - create charMap, then empty it - in case of permutation will be empty at the end + early exit
+
+            if(first.Length != second.Length) return false;
+
+            Dictionary<char, int> charMap = new();  //char, count
+
+            // fill char map from 1st string
+            foreach (char c in first) 
+            {
+                if (charMap.TryGetValue(c, out _))
+                    charMap[c]++;
+
+                else charMap[c] = 1;
+            }
+
+            //empty charmap from 2nd string
+            foreach (char c in second)
+            {
+                if (!charMap.TryGetValue(c, out _))
+                    return false;
+
+                else charMap[c]--;
+
+                if(charMap[c] == 0)
+                    charMap.Remove(c);
+            }
+
+            return charMap.Count == 0;
+            //Big O -> O(n+m)
+        }
+
+        public static bool IsPermutationOpt2(string first, string second)
+        {
+            //Naive - sorting strings then comparing them
+
+            if (first.Length != second.Length) return false;
+
+            char[] firstArr = first.ToCharArray();
+            char[] secondArr = second.ToCharArray();  
+
+            Array.Sort(firstArr);
+            Array.Sort(secondArr);
+
+            first = string.Concat(firstArr);
+            second = string.Concat(secondArr);
+
+            return first == second;
+            
+            
+            //Big O -> O(n*logn + m*logm)
+
+        }
+
+    }
+}
---
+++ tests/Study.CrackingTheCodingInterview.UnitTests/Ch1_ArraysAndStrings/Q2_CheckPermutationTests.cs
@@ -0,0 +1,117 @@
+﻿using Study.CrackingTheCodingInterview.Ch1_ArraysAndStrings;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Study.CrackingTheCodingInterview.UnitTests.Ch1_ArraysAndStrings
+{
+    public class Q2_CheckPermutationTests
+    {
+        private readonly (string first, string second) _validInput = ("valid", "dilav");
+        private readonly (string first, string second) _invalidInput = ("valid", "tests");
+        private readonly (string first, string second) _diffLenghts = ("valid", "validd");
+        private readonly (string first, string second) _singleCharInput = ("a", "a");
+
+        [Fact]
+        public void IsPermutationOpt1_ValidInput_ReturnsTrue() 
+        { 
+            //arrange
+            // -------
+
+            //act
+            var result = Q2_CheckPermutation.IsPermutationOpt1(_validInput.first, _validInput.second);
+
+            //assert
+            Assert.True(result);
+        }
+        [Fact]
+        public void IsPermutationOpt1_InvalidInput_ReturnsFalse()
+        {
+            //arrange
+            // -------
+
+            //act
+            var result = Q2_CheckPermutation.IsPermutationOpt1(_invalidInput.first, _invalidInput.second);
+
+            //assert
+            Assert.False(result);
+        }
+        [Fact]
+        public void IsPermutationOpt1_DiffLengths_ReturnsFalse()
+        {
+            //arrange
+            // -------
+
+            //act
+            var result = Q2_CheckPermutation.IsPermutationOpt1(_diffLenghts.first, _diffLenghts.second);
+
+            //assert
+            Assert.False(result);
+        }
+        [Fact]
+        public void IsPermutationOpt1_SingleCharInput_ReturnsTrue()
+        {
+            //arrange
+            // -------
+
+            //act
+            var result = Q2_CheckPermutation.IsPermutationOpt1(_singleCharInput.first, _singleCharInput.second);
+
+            //assert
+            Assert.True(result);
+        }
+
+
+
+
+        [Fact]
+        public void IsPermutationOpt2_ValidInput_ReturnsTrue()
+        {
+            //arrange
+            // -------
+
+            //act
+            var result = Q2_CheckPermutation.IsPermutationOpt2(_validInput.first, _validInput.second);
+
+            //assert
+            Assert.True(result);
+        }
+        [Fact]
+        public void IsPermutationOpt2_InvalidInput_ReturnsFalse()
+        {
+            //arrange
+            // -------
+
+            //act
+            var result = Q2_CheckPermutation.IsPermutationOpt2(_invalidInput.first, _invalidInput.second);
+
+            //assert
+            Assert.False(result);
+        }
+        [Fact]
+        public void IsPermutationOpt2_DiffLengths_ReturnsFalse()
+        {
+            //arrange
+            // -------
+
+            //act
+            var result = Q2_CheckPermutation.IsPermutationOpt2(_diffLenghts.first, _diffLenghts.second);
+
+            //assert
+            Assert.False(result);
+        }
+        [Fact]
+        public void IsPermutationOpt2_SingleCharInput_ReturnsTrue()
+        {
+            //arrange
+            // -------
+
+            //act
+            var result = Q2_CheckPermutation.IsPermutationOpt2(_singleCharInput.first, _singleCharInput.second);
+
+            //assert
+            Assert.True(result);
+        }
+
+    }
+}
