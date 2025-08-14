40d7e3cda6c424c52db501d9bb4ac70ae6def8ef
Brozda <daniel.brozik@gmail.com>
2025-08-14 16:25:36 +0200

    Q5 implemented and tested

---
+++ src/Study.CrackingTheCodingInterview/Ch1_ArraysAndStrings/Q4_PalindromePermutation.cs
@@ -0,0 +1,63 @@
+﻿namespace Study.CrackingTheCodingInterview.Ch1_ArraysAndStrings
+{
+    public static class Q4_PalindromePermutation
+    {
+        //given input - check if its permutation of palindrome - palindrome does not need to be limitedto dictionary letters
+        //Contraints: Ignore casing and non-letter chars
+        // example: Tact Coa -> True; Eg: taco cat, atco cta,....
+        public static bool IsPalindromePermutation(string input)
+        {
+            if(input.Length == 0)
+            {
+                return false;
+            }
+
+            int letterCount = 0;
+            Dictionary<char, int> charMap = new Dictionary<char, int>();
+
+            foreach (char c in input) 
+            {
+                if (char.IsLetter(c)) 
+                {
+                    if (!charMap.ContainsKey(c)) 
+                        charMap.Add(c, 1);
+                    else
+                        charMap[c]++;
+
+                    letterCount++;
+                }
+            }
+
+            // if input len is even - each character needs to be even
+            // if input len is odd - each but one chars have to be even, one can be odd
+            if (letterCount % 2 == 0)
+            {
+                foreach(var key in charMap.Keys)
+                {
+                    if (charMap[key] %2 != 0 )
+                        return false;
+                }
+            }
+            else
+            {
+                bool haveOdd = false;
+
+                foreach (var key in charMap.Keys)
+                {
+                    if (charMap[key] % 2 != 0)
+                    {
+                        if (!haveOdd)
+                            haveOdd= true;
+                        else
+                            return false;
+                    }
+                        
+                }
+
+            }
+
+            return true;
+             
+        }
+    }
+}
---
+++ tests/Study.CrackingTheCodingInterview.UnitTests/Ch1_ArraysAndStrings/Q4_PalindromePermutationTests.cs
@@ -0,0 +1,34 @@
+﻿using Study.CrackingTheCodingInterview.Ch1_ArraysAndStrings;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+
+namespace Study.CrackingTheCodingInterview.UnitTests.Ch1_ArraysAndStrings
+{
+    public class Q4_PalindromePermutationTests
+    {
+        private readonly string _validInput = "taco cat";
+        private readonly string _invalidInput = "coat";
+
+        [Fact]
+        public void IsPalindromePermutation_ValidInput_ReturnsTrue()
+        {
+            //arrange
+            //act
+            var result = Q4_PalindromePermutation.IsPalindromePermutation(_validInput);
+            //assert
+            Assert.True(result);
+        }
+        [Fact]
+        public void IsPalindromePermutation_InvalidInput_ReturnsFalse()
+        {
+            //arrange
+            //act
+            var result = Q4_PalindromePermutation.IsPalindromePermutation(_invalidInput);
+            //assert
+            Assert.False(result);
+        }
+
+    }
+}
